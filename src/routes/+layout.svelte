0f9f7ce665a35f9c5847f6b5951f0915c277c2b5
Richard Walker <richard.walker.90@gmail.com>
2026-01-22 11:44:03 -0500

    refactor: move header component into feature folder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-	import Header from '$lib/components/Header.svelte';
+	import Header from '$lib/components/features/Header.svelte';
 	import '../app.css';
 
 	let { children } = $props();
