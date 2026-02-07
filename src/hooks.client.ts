1f8a0925249618872130f80b8655005f7327d859
Richard Walker <richard.walker.90@gmail.com>
2026-02-07 16:29:05 -0500

    ops: add Sentry for error tracing
    Issues:
    closes #48

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,16 +1,23 @@
+import * as Sentry from '@sentry/sveltekit';
 import posthog from 'posthog-js';
 import { PUBLIC_POSTHOG_KEY } from '$env/static/public';
 import type { ClientInit } from '@sveltejs/kit';
 
 export const init: ClientInit = async () => {
-	await initMocks();
+	const environment = getEnvironment().toLowerCase();
+	console.log('Environment:', environment);
+
+	await initMocks(environment);
 	initializePostHog();
+	initializeSentry(environment);
 };
 
-async function initMocks() {
-	if (import.meta.env.DEV) {
+async function initMocks(environment: string) {
+	const isDev = environment === 'development';
+	if (isDev) {
 		const { worker } = await import('$lib/mocks/browser');
 		await worker.start({ onUnhandledRequest: 'bypass' });
+		console.log('Mock APIs initialized');
 	}
 }
 
@@ -19,4 +26,24 @@ function initializePostHog() {
 		api_host: 'https://us.i.posthog.com',
 		capture_pageview: false
 	});
+	console.log('Analytics initialized');
 }
+
+function initializeSentry(environment: string) {
+	// If you don't want to use Session Replay, remove the `Replay` integration,
+	// `replaysSessionSampleRate` and `replaysOnErrorSampleRate` options.
+	Sentry.init({
+		dsn: 'https://1e60c3df69dcb5c0146d459661e68ec7@o4510846661754880.ingest.us.sentry.io/4510846664769536',
+		tracesSampleRate: 1,
+		sendDefaultPii: true,
+		environment: environment
+	});
+	console.log('Telemetry initialized');
+}
+
+function getEnvironment(): string {
+	const environment: string = import.meta.env.MODE;
+	return environment;
+}
+
+export const handleError = Sentry.handleErrorWithSentry();
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), svelteTesting()],
+	plugins: [
+		sentrySvelteKit({
+			org: 'jaccountable',
+			project: 'jaccountable-frontend',
+			telemetry: false
+		}),
+		tailwindcss(),
+		sveltekit(),
+		svelteTesting()
+	],
 	test: {
 		environment: 'jsdom',
 		setupFiles: ['./vitest-setup.ts'],
